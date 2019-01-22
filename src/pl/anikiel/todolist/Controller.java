4c9920f8b0e184c76e6fd5166776e11077da09ea
Adrian Nikiel <hunter86@onet.eu>
2019-01-22 07:07:01 +0100

    typo fixed

--- src/pl/anikiel/todolist/Controller.java
+++ src/pl/anikiel/todolist/Controller.java
@@ -16,7 +16,7 @@ public class Controller {
     @FXML
     private ListView<TodoItem> todoListView;
 
-mai
+
     public void initialize() {
         TodoItem item1 = new TodoItem("Mail birthday card", "Buy a 30th birthday card for John",
                 LocalDate.of(2016, Month.APRIL, 25));
